33edebfc5b4c02c9e1eecbfbea41f1f59fcc797f
william koller <wkoller25@gmail.com>
2021-02-09 20:10:51 -0300

    chore: add Swagger and configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from '@/app.module';
+import { LoggingInterceptor } from '@/common/interceptors/logging.interceptor';
+import * as rateLimit from 'express-rate-limit';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.setGlobalPrefix('api');
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Sample Craft Beer Guide')
+    .setDescription('API of Craft Beer Guide')
+    .setVersion('1.0')
+    .addTag('style')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
