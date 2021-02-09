29afd647e11f2484e3ec3075de60a7a5ca5e612c
william koller <wkoller25@gmail.com>
2021-02-09 16:53:10 -0300

    chore: add configs of husky, lint-staged and StyleModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from '@/config/config.service';
+import { StyleModule } from '@/style/style.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0])],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
+    forwardRef(() => StyleModule),
+  ],
   controllers: [],
   providers: [],
 })
