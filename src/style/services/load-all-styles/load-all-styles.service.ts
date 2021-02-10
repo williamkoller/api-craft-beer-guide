ea9a2f3acab7a19c9a211df10c187a709c9f09f6
william koller <wkoller25@gmail.com>
2021-02-09 22:50:51 -0300

    feat: add conditional in case not found style

--- src/style/services/load-all-styles/load-all-styles.service.ts
+++ src/style/services/load-all-styles/load-all-styles.service.ts
@@ -1,12 +1,18 @@
 import { Style } from '@/entities/style.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { LoadAllStylesRepository } from '@/style/repositories/load-all-styles/load-all-styles.repository';
 
 @Injectable()
 export class LoadAllStylesService {
-  constructor(private readonly loadAllStylesRepository: LoadAllStylesRepository) {}
+  constructor(
+    private readonly loadAllStylesRepository: LoadAllStylesRepository,
+  ) {}
 
   async loadAll(): Promise<Array<Style>> {
-    return await this.loadAllStylesRepository.loadAllStyles();
+    const styles = await this.loadAllStylesRepository.loadAllStyles();
+    if (styles?.length === 0) {
+      throw new NotAcceptableException('No records found.');
+    }
+    return styles;
   }
 }
