55aaa5c445950940688084c899243b47a1432cc6
william koller <wkoller25@gmail.com>
2021-02-09 21:59:11 -0300

    feat: add CategoryModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from '@/config/config.service';
 import { StyleModule } from '@/style/style.module';
+import { CategoryModule } from '@/category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), forwardRef(() => StyleModule)],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
+    forwardRef(() => StyleModule),
+    forwardRef(() => CategoryModule),
+  ],
   controllers: [],
   providers: [],
 })
