fe625ffd91d2d8049acf77fd05bff4c6bc4be744
william koller <wkoller25@gmail.com>
2021-02-11 00:26:30 -0300

    refactor: alter logic if style name exists

--- src/style/services/add-style/add-style.service.ts
+++ src/style/services/add-style/add-style.service.ts
@@ -2,18 +2,18 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { Style } from '@/entities/style.entity';
 import { AddStyleDto } from '@/style/dtos/add-style/add-style.dto';
 import { AddStyleRepository } from '@/style/repositories/add-style/add-style.repository';
-import { LoadStyleByNameRepository } from '@/style/repositories/load-style-by-name/load-style-by-name.repository';
+import { LoadStyleByNameService } from '../load-style-by-name/load-style-by-name.service';
 
 @Injectable()
 export class AddStyleService {
   constructor(
     private readonly addStyleRepository: AddStyleRepository,
-    private readonly loadStyleByNameRepository: LoadStyleByNameRepository,
+    private readonly loadStyleByNameService: LoadStyleByNameService,
   ) {}
 
   async addStyle(addStyleDto: AddStyleDto): Promise<Style> {
     const { name } = addStyleDto;
-    const style = await this.loadStyleByNameRepository.loadStyleByName(name);
+    const [style] = await this.loadStyleByNameService.loadByName(name);
     if (style) {
       throw new ConflictException(
         'There is already a record with a name for this style.',
