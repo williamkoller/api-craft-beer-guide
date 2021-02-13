a034f50611b45f8e0e3cc1001085302c4fee529a
william koller <wkoller25@gmail.com>
2021-02-13 15:04:03 -0300

    feat: add UserModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { configService } from '@/config/config.service';
 import { StyleModule } from '@/style/style.module';
 import { CategoryModule } from '@/category/category.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '@/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
     forwardRef(() => StyleModule),
     forwardRef(() => CategoryModule),
+    forwardRef(() => UserModule),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
