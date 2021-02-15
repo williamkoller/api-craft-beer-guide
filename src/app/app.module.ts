ef9d5980b875e0faf3662f5956e4401fa8631fbe
william koller <wkoller25@gmail.com>
2021-02-15 02:10:22 -0300

    feat: add AuthModule and passport-jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { StyleModule } from '@/style/style.module';
 import { CategoryModule } from '@/category/category.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '@/user/user.module';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from '@/user/user.module';
     forwardRef(() => StyleModule),
     forwardRef(() => CategoryModule),
     forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
