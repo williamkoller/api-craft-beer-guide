2e95cfb98abac3bdea079094d3b1443b0cc4a612
william koller <wkoller25@gmail.com>
2021-02-14 22:52:53 -0300

    feat: add CryptHashEncrypt, CryptHashCompare in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddUserRepository } from '@/user/repositories/add-user/add-user.repository';
 import { AddUserService } from '@/user/services/add-user/add-user.service';
 import { AddUserController } from '@/user/controllers/add-user/add-user.controller';
+import { CryptHashEncrypt } from '@/shared/crypt-hash/crypt-hash-encrypt/crypt-hash-encrypt';
+import { CryptHashCompare } from '@/shared/crypt-hash/crypt-hash-compare/crypt-hash-compare';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, AddUserRepository])],
-  providers: [AddUserService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [
+    AddUserService,
+    CryptHashEncrypt,
+    CryptHashCompare,
+    AddUserRepository,
+  ],
   controllers: [AddUserController],
 })
 export class UserModule {}
