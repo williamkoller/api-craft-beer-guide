783236baa6bd1a40f8af25138a3cc3167ad52d72
william koller <wkoller25@gmail.com>
2021-02-14 22:48:22 -0300

    refactor: add bcryptHashEncrypt in password

--- src/user/repositories/add-user/add-user.repository.ts
+++ src/user/repositories/add-user/add-user.repository.ts
@@ -1,13 +1,26 @@
 import { User } from '@/entities/user.entity';
+import { CryptHashEncrypt } from '@/shared/crypt-hash/crypt-hash-encrypt/crypt-hash-encrypt';
 import { AddUserDto } from '@/user/dtos/add-user/add-user.dto';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { validateOrReject } from 'class-validator';
-import { EntityRepository, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 
-@EntityRepository(User)
-export class AddUserRepository extends Repository<User> {
+@Injectable()
+export class AddUserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly addUserRepository: Repository<User>,
+    private readonly cryptHashEncrypt: CryptHashEncrypt,
+  ) {}
   async addUser(addUserDto: AddUserDto): Promise<User> {
     const user = Object.assign({} as User, addUserDto);
-    validateOrReject(user);
-    return await this.save(user);
+    const userCreated = this.addUserRepository.create({
+      fullName: user.fullName,
+      email: user.email,
+      password: await this.cryptHashEncrypt.encrypt(user.password),
+    });
+    validateOrReject(userCreated);
+    return await this.addUserRepository.save(userCreated);
   }
 }
