c924431f1e52bb65f01c230e84d7d76576b0d26f
william koller <wkoller25@gmail.com>
2021-02-16 17:50:15 -0300

    feat: add validateUser in validateUserService

--- src/auth/services/validate-user/validate-user.service.ts
+++ src/auth/services/validate-user/validate-user.service.ts
@@ -1,9 +1,18 @@
+import { User } from '@/entities/user.entity';
 import { LoadUserByEmailService } from '@/user/services/load-user-by-email/load-user-by-email.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class ValidateUserService {
   constructor(
     private readonly loadUserByEmailService: LoadUserByEmailService,
   ) {}
+
+  async validateUser(email: User['email']): Promise<User> {
+    const user = await this.loadUserByEmailService.loadUserByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
 }
