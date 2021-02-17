ba5d77a8f8736e4aceb9d29146a7c49a1743be17
william koller <wkoller25@gmail.com>
2021-02-16 23:05:55 -0300

    feat: add ModelNotFoundException

---
+++ src/common/filters/model-not-found.exception.filter.ts
@@ -0,0 +1,17 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Response } from 'express';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
+
+@Catch(EntityNotFoundError)
+export class ModelNotFoundException implements ExceptionFilter {
+  catch(exception: EntityNotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    return response.status(404).json({
+      error: {
+        error: 'Not Found',
+        message: exception.message,
+      },
+    });
+  }
+}
