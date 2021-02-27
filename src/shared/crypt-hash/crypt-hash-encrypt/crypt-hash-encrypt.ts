ea9ad25f4eaf4cbf1364c3eb62febff74924841f
william koller <wkoller25@gmail.com>
2021-02-27 02:49:46 -0300

    refactor: rename env in salt

--- src/shared/crypt-hash/crypt-hash-encrypt/crypt-hash-encrypt.ts
+++ src/shared/crypt-hash/crypt-hash-encrypt/crypt-hash-encrypt.ts
@@ -3,7 +3,7 @@ import { hashSync } from 'bcrypt';
 
 @Injectable()
 export class CryptHashEncrypt {
-  private readonly salt = Number(process.env.saltOrRounds);
+  private readonly salt = Number(process.env.SALT_OR_ROUNDS);
 
   async encrypt(password: string): Promise<string> {
     return hashSync(password, this.salt);
